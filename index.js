42474b78569e4ea8f5f7b3027b2cb81f9513d958
silvianurrobianti <silvianurrobianti@gmail.com>
2024-11-18 03:34:23 +0700

    integrated with API local

--- index.js
+++ index.js
@@ -3,6 +3,20 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const productsRoute = require('./routes/productsRoute');
+const os = require('os');
+const interfaces = os.networkInterfaces();
+
+// Fungsi untuk mendapatkan IP lokal
+function getLocalIPAddress() {
+  for (const name of Object.keys(interfaces)) {
+    for (const net of interfaces[name]) {
+      if (net.family === 'IPv4' && !net.internal) {
+        return net.address;
+      }
+    }
+  }
+  return 'localhost';
+}
 
 // Initialize new express application instance
 const app = express();
@@ -22,8 +36,13 @@ if (!MONGODB_URI) {
   console.error("MONGODB_URI is not defined. Please check your .env file.");
 } else {
   mongoose.connect(MONGODB_URI)
-    .then(() => app.listen(PORT, () => console.log(`Connected to DB and running on http://localhost:${PORT}`)))
-    .catch((error) => console.log(`Error:`, error.message));
+  .then(() => {
+    const localIP = getLocalIPAddress();
+    app.listen(PORT, () => {
+      console.log(`Connected to DB and running on http://${localIP}:${PORT}`);
+    });
+  })
+  .catch((error) => console.log(`Error:`, error.message));
 }
 
 
